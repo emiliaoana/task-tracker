413cbab48e834c5e50c72c42eb14a355a76a59e2
emiliaoana <92299329+emiliaoana@users.noreply.github.com>
2023-03-15 21:26:30 +0200

    added tasks

---
+++ src/app/mock-task.ts
@@ -0,0 +1,21 @@
+import {Task} from "./Task"
+export const TASKS:Task[]=[
+  {
+    id:1,
+    text: 'Task 1',
+    day:'Day 1',
+    reminder:true,
+  },
+  {
+    id:2,
+    text: 'Task 2',
+    day:'Day 2',
+    reminder:true,
+  },
+  {
+    id:3,
+    text: 'Task 3',
+    day:'Day 3',
+    reminder:false,
+  },
+]
