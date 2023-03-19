c3b46748aa6963bd5adf5af42ee9b8bbb9646038
emiliaoana <92299329+emiliaoana@users.noreply.github.com>
2023-03-19 23:22:28 +0200

    added a random route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import{HttpClientModule} from "@angular/common/http";
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TasksItemComponent } from './components/tasks-item/tasks-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { AddTaskComponent } from './components/add-task/add-task.component';
-import{FormsModule} from "@angular/forms";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {ButtonComponent} from './components/button/button.component';
+import {TasksComponent} from './components/tasks/tasks.component';
+import {TasksItemComponent} from './components/tasks-item/tasks-item.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {AddTaskComponent} from './components/add-task/add-task.component';
+import {FormsModule} from "@angular/forms";
+import {Router, RouterModule, Routes} from "@angular/router";
+import {RouterComponent} from './components/router/router.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'greeting', component: RouterComponent}
+
+]
 
 @NgModule({
   declarations: [
@@ -17,15 +25,18 @@ import{FormsModule} from "@angular/forms";
     ButtonComponent,
     TasksComponent,
     TasksItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    RouterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
